290754eaae43c80db72bb1864ba65f9e5183a963
Brookins Consulting <info@brookinsconsulting.com>
2007-05-23 22:29:33 +0000

    Added: doc/doxygen configuration file and updated ezinfo.php project url, autoload doxygen comments

--- settings/wrap_operator.ini.append.php
+++
@@ -1,7 +0,0 @@
-# Names of callable user defined functions for wrap_user_func.
-# Note that they must be defined in separate files under userfunctions directory.
-[UserFunctions]
-## PermittedFunctionList[]
-PermittedFunctionList[]=escapeJsString
-PermittedFunctionList[]=getFormatNumericDecimal
-PermittedFunctionList[]=getXMLString

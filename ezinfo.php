e4d177e915eac35fa89ed290851cc293546aa426
Brookins Consulting <info@brookinsconsulting.com>
2007-05-24 04:12:08 +0000

    Updated: release version number in ezinfo.php

--- ezinfo.php
+++ ezinfo.php
@@ -5,7 +5,7 @@ class googleanalyticsInfo
     {
         return array(
             'Name' => "<a href='http://projects.ez.no/googleanalytics'>Google Analytics</a>",
-            'Version' => "1.0.0",
+            'Version' => "1.0.1",
             'Copyright' => "Copyright (C) 2001 - 2007 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
             'Author' => "Brookins Consulting",
             'License' => "GNU General Public License"

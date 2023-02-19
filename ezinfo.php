9f1e9fe11f2447ca12edf60e6629bfa4fadb0297
Brookins Consulting <info@brookinsconsulting.com>
2023-02-19 01:34:57 +0000

    Updated: Updated ezinfo to replace projects with github for project link and bump version number

--- ezinfo.php
+++ ezinfo.php
@@ -4,9 +4,9 @@ class bcwebsitestatisticsInfo
     function info()
     {
         return array(
-            'Name' => "<a href='http://projects.ez.no/bcwebsitestatistics'>BC Website Statistics</a>",
-            'Version' => "1.0.3",
-            'Copyright' => "Copyright (C) 2001 - 2007 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
+            'Name' => "<a href='https://github.com/brookinsconsulting/bcwebsitestatistics'>BC Website Statistics</a>",
+            'Version' => "1.0.4",
+            'Copyright' => "Copyright (C) 2001 - 2023 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
             'Author' => "Brookins Consulting",
             'License' => "GNU General Public License"
         );
